2cbc3b69a544b0f856939025554b8d19a130d489
Nicholas Mahlangu <cubemasternm@gmail.com>
2016-02-24 03:42:33 -0500

    all messages are now group messages. all groups now appear on left side like facebook messenger

--- PROT/chat/chat.js
+++ PROT/chat/chat.js
@@ -8,13 +8,14 @@ var recepientTypes = {
 		GROUP: 1
 };
 
-// REMOVE: empty accounts and create 3 dummy users
+// REMOVE AFTER TESTING 
+// empty accounts and create 3 dummy users
 function resetAccounts() {
 	// drop table
 	Meteor.users.remove({});
 
 	// create new users
-	for (var i = 0; i < 3; i++) {
+	for (var i = 0; i < 4; i++) {
 		Accounts.createUser({
 			username: "testuser" + (i + 1).toString(),
 			password: "testpassword" + (i + 1).toString()
@@ -22,43 +23,48 @@ function resetAccounts() {
 	}
 }
 
-// REMOVE: send 3 "account" messages from testuser2 to testuser1
-function sendTestAccountMessages() {
-	// drop table
+// REMOVE AFTER TESTING
+// send 3 "group" messages from testuser2 to testuser1
+// send 3 "group" messages from testuser2 to testuser1 and testuser3
+// send 3 "group" messages from testuser1 to testuser 2, 3, and 4
+function sendTestGroupMessages() {
+	// drop tables
+	Groups.remove({});
 	Messages.remove({});
 
 	// test accounts
 	var testuser1 = Meteor.users.findOne({username: "testuser1"});
 	var testuser2 = Meteor.users.findOne({username: "testuser2"});
 	var testuser3 = Meteor.users.findOne({username: "testuser3"});
+	var testuser4 = Meteor.users.findOne({username: "testuser4"});
+
+	// get a new group id number
+	var newGroupId = Random.id();
 
 	// insert messages
+	// send 3 "group" messages from testuser2 to testuser1
 	for (var i = 0; i < 3; i++) {
 		Messages.insert({
-			recipientId: testuser1._id,
-			recipientType: recepientTypes.ACCOUNT,
+			recipientId: newGroupId,
+			recipientType: recepientTypes.GROUP,
 			id: Random.id(), 
 			senderId: testuser2._id,
 			createdAt: new Date(),
 			content: "Message " + (i + 1).toString() + " from testuser2 to testuser1"
 		});
 	}
-}
 
-// REMOVE: send 3 "group" messages from testuser2 to testuser1 and testuser3
-function sendTestGroupMessages() {
-	// drop table
-	Groups.remove({});
+	// insert new group with corresponding groupId
+	Groups.insert({
+		groupId: newGroupId,
+		accountIds: [testuser1._id, testuser2._id]
+	});
 
-	// test accounts
-	var testuser1 = Meteor.users.findOne({username: "testuser1"});
-	var testuser2 = Meteor.users.findOne({username: "testuser2"});
-	var testuser3 = Meteor.users.findOne({username: "testuser3"});
-
-	// get a new group id numbers
-	var newGroupId = Random.id();
+	// get a new group id number
+	newGroupId = Random.id();
 
 	// insert messages
+	// send 3 "group" messages from testuser2 to testuser1 and testuser3
 	for (var i = 0; i < 3; i++) {
 		Messages.insert({
 			recipientId: newGroupId,
@@ -75,13 +81,59 @@ function sendTestGroupMessages() {
 		groupId: newGroupId,
 		accountIds: [testuser1._id, testuser2._id, testuser3._id]
 	});
+
+	// get a new group id number
+	newGroupId = Random.id();
+
+	// insert messages
+	// send 3 "group" messages from testuser1 to testuser 2, 3, and 4
+	for (var i = 0; i < 3; i++) {
+		Messages.insert({
+			recipientId: newGroupId,
+			recipientType: recepientTypes.GROUP,
+			messageId: Random.id(), 
+			senderId: testuser1._id,
+			createdAt: new Date(),
+			content: "Message " + (i + 1).toString() + " from testuser1 to testuser2, testuser3, testuser4"
+		});
+	}
+
+	// insert new group with corresponding groupId
+	Groups.insert({
+		groupId: newGroupId,
+		accountIds: [testuser1._id, testuser2._id, testuser3._id, testuser4._id]
+	});
+}
+
+// returns an array of group objects the user is in
+function getGroupsUserIsIn() {
+	groupsUserIsIn = Groups.find({}).fetch();
+	groupsUserIsIn = groupsUserIsIn.filter(function(d) {
+		return d.accountIds.indexOf(Meteor.userId()) != -1;
+	})
+	return groupsUserIsIn;
+}
+
+// creates a group name just like facebook messenger
+function createGroupNameFromGroupObj(obj) {
+	var groupName = "";
+	obj.accountIds.forEach(function(id) {
+		if (id != Meteor.userId()) {
+			if (groupName == "") {
+				groupName = Meteor.users.findOne({_id: id}).username;
+			}
+			else {
+				groupName += ", " + Meteor.users.findOne({_id: id}).username;
+			}
+		}	
+	});
+	return groupName;
 }
 
 // server
 if (Meteor.isServer) {
 	// testing init
 	// resetAccounts();
-	sendTestAccountMessages();
 	sendTestGroupMessages();
 }
 
@@ -95,7 +147,7 @@ if (Meteor.isClient) {
     	passwordSignupFields: 'USERNAME_ONLY'
   	});
 
-	// event handlers
+	// body event handlers
 	Template.body.events({
 		"click .logCurrentUser": function(event) {
       		event.preventDefault();	// don't submit
@@ -104,18 +156,36 @@ if (Meteor.isClient) {
 		},
 		"click .logUserAccountMessages": function(event) {
 			event.preventDefault(); // don't submit
-			userMessages = Messages.find({recipientId: Meteor.userId()}, {sort: {ts: -1}});
-			console.log("Here is a log of the current user's messages: ");
+			var userMessages = Messages.find({recipientId: Meteor.userId()}, {sort: {ts: -1}});
+			console.log("Here is a log of the current user's 'account' messages: ");
 			console.log(userMessages.fetch());
 		},
-		"click .logUserGroupMessages": function(event) {
+		"click .logUserGroups": function(event) {
 			event.preventDefault(); // don't submit
 			groupsUserIsIn = Groups.find({}).fetch();
 			groupsUserIsIn = groupsUserIsIn.filter(function(d) {
 				return d.accountIds.indexOf(Meteor.userId()) != -1;
 			});
+			console.log("Here is a log of the groups the current user is in: ");
 			console.log(groupsUserIsIn);
 		}
+    });
+
+    // groups
+     Template.body.helpers({
+    	groups: function() {
+			var groupsUserIsIn = getGroupsUserIsIn();
+    		var groupNames = [];
+    		groupsUserIsIn.forEach(function(group) {
+    			groupNames.push(createGroupNameFromGroupObj(group));
+    		});
+    		returnVal = []
+    		groupNames.forEach(function(d) {
+    			var obj = {groupName: d};
+    			returnVal.push(obj);
+    		});
+    		return returnVal;
+    	}
     });
 }
 
